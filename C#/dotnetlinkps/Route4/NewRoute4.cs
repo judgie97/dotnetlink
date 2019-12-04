9853c2bfbfe2ee70560a61fd56af1e12c7163aa5
Ellis Judge <mail@ellisjudge.com>
2019-12-04 23:25:29 +0000

    Added New-Route4 cmdlet
    Added New-Route4 cmdlet to allow routes to be created for the Add-Route4 cmdlet

---
+++ C#/dotnetlinkps/Route4/NewRoute4.cs
@@ -0,0 +1,39 @@
+using System.Management.Automation;
+using System.Net;
+using dotnetlink;
+
+namespace dotnetlinkps
+{
+    [Cmdlet(VerbsCommon.New, "Route4")]
+    public class NewRoute4 : PSCmdlet
+    {
+        [Parameter]
+        public IPAddress destination;
+        
+        [Parameter]
+        public IPAddress gateway;
+        
+        [Parameter]
+        public byte netmask;
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing()
+        {
+            WriteObject(new Route4(destination, netmask, gateway, 0));
+        }
+
+        protected override void StopProcessing()
+        {
+            base.StopProcessing();
+        }
+    }
+}
